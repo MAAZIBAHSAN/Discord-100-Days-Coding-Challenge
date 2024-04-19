21394064058f57311c4fd743a5bd7478765f6abb
M.AAZIBAHSAN <160249107+MAAZIBAHSAN@users.noreply.github.com>
2024-04-19 16:59:58 +0500

    Update: add readme file eachday

---
+++ DAY-02/q5/main.js
@@ -0,0 +1,3 @@
+var myName = "\t\n Aazib \t\n"; // this save this name with whitespace
+console.log(myName); // show the name with white space
+console.log(myName.trim()); // show the name without white space
---
+++ DAY-02/q5/main.ts
@@ -0,0 +1,4 @@
+let myName: string = "\t\n Aazib \t\n"; // this save this name with whitespace
+console.log(myName); // show the name with white space
+console.log(myName.trim()) // show the name without white space
+
