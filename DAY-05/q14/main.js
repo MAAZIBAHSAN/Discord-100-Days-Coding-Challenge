b3135fdb6922935bfe9934f9fd383c996cd69423
M.AAZIBAHSAN <160249107+MAAZIBAHSAN@users.noreply.github.com>
2024-06-05 02:12:34 +0500

    👉 update 😃

--- DAY-05/q14/main.js
+++ DAY-05/q14/main.js
@@ -2,6 +2,8 @@
 //=================-=================(Today Is Day No 5)=================-=================//
 //=====(Explain & TIP: Just like with transportation, you can use an array to manage a list of guests. Loop through the list to send each one a personal invitation.)=====//
 //====================-====================(Q14)====================-====================//
+//=====(Explain & TIP: Just like with transportation, you can use an array to manage a list of guests. Loop through the list to send each one a personal invitation.)=====//
+// question no 14
 var guests = ["Asharib Ali", "Hunain Hyder", "Ahmed sheikh"];
 guests.forEach(function (guests) {
     console.log("Dear ".concat(guests, ", would you like to join me for dinner?"));
